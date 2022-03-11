6bbe907e91163886963b7cc8277ae790af8d2097
DYSLEVIUM <pushpakantbehera@hotmail.com>
2022-03-11 23:29:21 +0530

    added movement

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 cpp_version = c++2a
 src_file = ./src/main.cc
 executable = ./src/main.exe
-deps = ./deps/SFML/SFML-2.5.1
+deps = deps/SFML/SFML-2.5.1
 
 run: $(executable)
-	./$(executable)
+	$(executable) > output.log 2> error.log
 
 $(executable): $(src_file) $(deps)
-	g++ -std=$(cpp_version) $(src_file) -L $(deps) -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network -o $(executable)
+	g++ -std=$(cpp_version) -H $(src_file) -I $(deps)/include -L $(deps)/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network -o $(executable)
 
 clean:
 	rm -rf $(executable)
--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,11 @@
-#include <stdio.h>
-
 #include <SFML/Graphics.hpp>
+#include <cstdio>
+#include <memory>
 
-constexpr int WIDTH = 620, HEIGHT = 360;
-constexpr int MAX_ITERATION = 128;
-constexpr int INFINITY = 4;
+constexpr int WIDTH = 1280, HEIGHT = 720;
+constexpr long double ASPECT_RATIO = WIDTH / HEIGHT;
+constexpr int MAX_ITERATION = 256;
+constexpr int INFINITY = 32;
 constexpr long double ZOOM_FACTOR = 2;
 
 long double min_re = -2.5, max_re = 1;
@@ -16,8 +17,8 @@ inline T squared(const T& x) {
 }
 
 int main() {
-  sf::RenderWindow* window =
-      new sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot Set");
+  std::unique_ptr<sf::RenderWindow> window(
+      new sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot Set"));
 
   //  center the window
   window->setPosition(sf::Vector2i(
@@ -30,36 +31,57 @@ int main() {
   sf::Texture texture;
   sf::Sprite sprite;
 
+  auto zoom = [&](const long double& pos_x, const long double& pos_y,
+                  const long double& z) {
+    //  changing the center to the mouse click point
+    long double re_0 = min_re + (max_re - min_re) * pos_x / WIDTH;
+    long double im_0 = min_im + (max_im - min_im) * pos_y / HEIGHT;
+
+    //  zoom
+    long double scaled_min_re = re_0 - (max_re - min_re) / 2.0 / z;
+    long double scaled_max_re = re_0 + (max_re - min_re) / 2.0 / z;
+    min_re = scaled_min_re;
+    max_re = scaled_max_re;
+
+    long double scaled_min_im = im_0 - (max_im - min_im) / 2.0 / z;
+    long double scaled_max_im = im_0 + (max_im - min_im) / 2.0 / z;
+    min_im = scaled_min_im;
+    max_im = scaled_max_im;
+  };
+
   while (window->isOpen()) {
     sf::Event event;
     while (window->pollEvent(event)) {
       if (event.type == sf::Event::Closed) window->close();
+
+      if (event.type == sf::Event::KeyPressed) {
+        //  move delta
+        long double x_delta = (max_re - min_re) * ASPECT_RATIO * 0.3;
+        long double y_delta = (max_im - min_im) * (1.0 / ASPECT_RATIO) * 0.3;
+
+        if (event.key.code == sf::Keyboard::Left ||
+            event.key.code == sf::Keyboard::A) {
+          min_re -= x_delta, max_re -= x_delta;
+        } else if (event.key.code == sf::Keyboard::Right ||
+                   event.key.code == sf::Keyboard::D) {
+          min_re += x_delta, max_re += x_delta;
+        } else if (event.key.code == sf::Keyboard::Up ||
+                   event.key.code == sf::Keyboard::W) {
+          min_im -= y_delta, max_im -= y_delta;
+        } else if (event.key.code == sf::Keyboard::Down ||
+                   event.key.code == sf::Keyboard::S) {
+          min_im += y_delta, max_im += y_delta;
+        }
+      }
+
       if (event.type == sf::Event::MouseButtonPressed) {
-        auto zoom = [&](long double z) {
-          //  changing the center to the mouse click point
-          long double re_0 =
-              min_re + (max_re - min_re) * event.mouseButton.x / WIDTH;
-          long double im_0 =
-              min_im + (max_im - min_im) * event.mouseButton.y / HEIGHT;
-
-          //  zoom
-          long double scaled_min_re = re_0 - (max_re - min_re) / 2.0 / z;
-          long double scaled_max_re = re_0 + (max_re - min_re) / 2.0 / z;
-          min_re = scaled_min_re;
-          max_re = scaled_max_re;
-
-          long double scaled_min_im = im_0 - (max_im - min_im) / 2.0 / z;
-          long double scaled_max_im = im_0 + (max_im - min_im) / 2.0 / z;
-          min_im = scaled_min_im;
-          max_im = scaled_max_im;
-        };
         //  left click to zoom in
         if (event.mouseButton.button == sf::Mouse::Left) {
-          zoom(ZOOM_FACTOR);
+          zoom(event.mouseButton.x, event.mouseButton.y, ZOOM_FACTOR);
         }
         //  right click to zoom out
         else if (event.mouseButton.button == sf::Mouse::Right) {
-          zoom(1.0 / ZOOM_FACTOR);
+          zoom(event.mouseButton.x, event.mouseButton.y, 1.0 / ZOOM_FACTOR);
         }
       }
     }
@@ -70,6 +92,7 @@ int main() {
 #pragma omp parallel for
     for (int y{}; y < HEIGHT; ++y) {
       for (int x{}; x < WIDTH; ++x) {
+        //  making this as a range from 0 to window.DIM
         const long double re_0 =
             min_re +
             (max_re - min_re) * x / WIDTH;  //  x - axis as the real axis
@@ -81,25 +104,25 @@ int main() {
         int iteration = 0;
         while (iteration++ < MAX_ITERATION) {
           long double curr_re = re;
+          //  z = z^2 + c
           re = squared(re) - squared(im) + re_0;
           im = 2 * curr_re * im + im_0;
 
           if (squared(re) + squared(im) > INFINITY) break;
         }
 
-        int r = 1.0 * (MAX_ITERATION - iteration) / MAX_ITERATION * 0xff;
+        //  settting the color value in the range of 0 to 255
+        int r = -1.0 * (MAX_ITERATION - iteration) / MAX_ITERATION * 0xff;
         int g = r, b = r;
 
         image.setPixel(x, y, sf::Color(r, g, b));
       }
     }
+
     texture.loadFromImage(image);
     sprite.setTexture(texture);
 
     window->draw(sprite);
-
-    std::printf("Done\n");
-    std::fflush(stdout);
     window->display();
   }
 
